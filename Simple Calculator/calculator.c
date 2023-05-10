935680a8cc7a20f1a6b3ef41c0afd075973e0d97
Nathan Hernandez <99368546+Nathan-Hernandez02@users.noreply.github.com>
2023-05-09 21:56:48 -0500

    Update calculator.c

--- Simple Calculator/calculator.c
+++ Simple Calculator/calculator.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
+// create a calculator that takes in user input and outputs a result
+int main(int argc, char * argv[]) {
+  
+}
